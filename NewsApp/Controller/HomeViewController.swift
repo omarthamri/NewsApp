d02674abe18704a19654c7b3d66f266f16fc75a8
Omar Thamri <omarthamri4@gmail.com>
2019-08-30 22:29:34 +0100

    #0001 implement HomeViewController

---
+++ NewsApp/Controller/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Omar Thamri on 30/08/2019.
+//  Copyright © 2019 MACBOOK PRO RETINA. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationController()
+    }
+    
+    func setupNavigationController() {
+        navigationItem.title = "NewsApp"
+    }
+    
+    
+
+}
+
