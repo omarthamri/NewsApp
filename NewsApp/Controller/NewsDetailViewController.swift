5a6bcd3c129d55094a03b2d4724cdca61c4b410b
Omar Thamri <omarthamri4@gmail.com>
2019-09-02 00:23:26 +0100

    #0004 implement homeDetailViewController

--- NewsApp/Controller/NewsDetailViewController.swift
+++ NewsApp/Controller/NewsDetailViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class NewsDetailViewController: UIViewController {
     
+    lazy var newDetailCV: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.delegate = self
+        cv.dataSource = self
+        return cv
+    }()
+    let newDetailCVCellId = "newDetailCVCellId"
+    let NewImgCollectionViewCellId = "NewImgCollectionViewCellId"
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
@@ -19,14 +30,49 @@ class NewsDetailViewController: UIViewController {
     
     func setupView() {
         view.backgroundColor = UIColor.white
+        view.addSubview(newDetailCV)
+        newDetailCV.register(UICollectionViewCell.self, forCellWithReuseIdentifier: newDetailCVCellId)
+        newDetailCV.register(NewImgCollectionViewCell.self, forCellWithReuseIdentifier: NewImgCollectionViewCellId)
     }
     
     func setupConstraints() {
-        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":newDetailCV]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":newDetailCV]))
     }
     
     func setupNavigationBar() {
         navigationController?.navigationBar.isHidden = true
     }
     
+    func backAction() {
+        navigationController?.navigationBar.isHidden = false
+        navigationController?.popViewController(animated: true)
+    }
+    
+}
+
+extension NewsDetailViewController: UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.item == 0 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NewImgCollectionViewCellId, for: indexPath) as! NewImgCollectionViewCell
+            cell.newsDetailViewController = self
+            return cell
+        }
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: newDetailCVCellId, for: indexPath)
+        cell.backgroundColor = UIColor.yellow
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height / 3)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
---
+++ NewsApp/View/NewImgCollectionViewCell.swift
@@ -0,0 +1,58 @@
+//
+//  NewImgCollectionViewCell.swift
+//  NewsApp
+//
+//  Created by Omar Thamri on 01/09/2019.
+//  Copyright © 2019 MACBOOK PRO RETINA. All rights reserved.
+//
+
+import UIKit
+
+class NewImgCollectionViewCell: UICollectionViewCell {
+    
+    let newImg: UIImageView = {
+       let newImg = UIImageView()
+        newImg.image = UIImage(named: "tim_cook")
+       newImg.contentMode = .scaleToFill
+        newImg.translatesAutoresizingMaskIntoConstraints = false
+        return newImg
+    }()
+    var newsDetailViewController: NewsDetailViewController? 
+    lazy var backBtn: UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setImage(UIImage(named: "back"), for: .normal)
+        btn.tintColor = UIColor.darkGray
+        btn.addTarget(self, action: #selector(backAction), for: .touchUpInside)
+        return btn
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+        setupConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        backgroundColor = UIColor.blue
+        addSubview(newImg)
+        addSubview(backBtn)
+    }
+    
+    func setupConstraints() {
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":newImg]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":newImg]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[v0(25)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":backBtn]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-20-[v0(25)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":backBtn]))
+    }
+    
+    @objc func backAction() {
+        newsDetailViewController?.backAction()
+    }
+    
+    
+}
